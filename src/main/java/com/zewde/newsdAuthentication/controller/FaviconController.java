533475fbe71bb2bd89737f454e9f2136e90f5db2
snzew <Sitan.zewde@code.berlin>
2021-04-24 07:12:31 +0200

    favicion

--- src/main/java/com/zewde/newsdAuthentication/controller/FaviconController.java
+++ src/main/java/com/zewde/newsdAuthentication/controller/FaviconController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class FaviconController {
-  @GetMapping("/favicon.ico")
+
+  @GetMapping("favicon.ico")
   @ResponseBody
   void returnNoFavicon() {
   }
