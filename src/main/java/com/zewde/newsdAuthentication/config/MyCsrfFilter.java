50eb340a4c363dd42523389427e0df20d2feb02e
snzew <Sitan.zewde@code.berlin>
2021-05-03 19:33:11 +0200

    test

---
+++ src/main/java/com/zewde/newsdAuthentication/config/MyCsrfFilter.java
@@ -0,0 +1,68 @@
+package com.zewde.newsdAuthentication.config;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class MyCsrfFilter extends OncePerRequestFilter {
+
+  private static final String CSRF_TOKEN = "CSRF-TOKEN";
+  private static final String X_CSRF_TOKEN = "X-CSRF-TOKEN";
+  private final RequestMatcher requireCsrfProtectionMatcher = new DefaultRequiresCsrfMatcher();
+  private final AccessDeniedHandler accessDeniedHandler = new AccessDeniedHandlerImpl();
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+                                  HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    if (requireCsrfProtectionMatcher.matches(request)) {
+      final String csrfTokenValue = request.getHeader(X_CSRF_TOKEN);
+      final Cookie[] cookies = request.getCookies();
+      System.out.println("***********************************************");
+      System.out.println("***********************************************");
+      System.out.println("***********************************************");
+      System.out.println("***********************************************");
+      System.out.println("***********************************************");
+      System.out.println("csrf token value    "+csrfTokenValue);
+
+      String csrfCookieValue = null;
+      if (cookies != null) {
+        for (Cookie cookie : cookies) {
+          if (cookie.getName().equals(CSRF_TOKEN)) {
+            csrfCookieValue = cookie.getValue();
+            System.out.println("csrf token cookie value "+csrfCookieValue);
+
+          }
+        }
+      }
+
+      if (csrfTokenValue == null || !csrfTokenValue.equals(csrfCookieValue)) {
+
+        accessDeniedHandler.handle(request, response, new AccessDeniedException(
+            "Missing or non-matching CSRF-token"));
+        return;
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
+
+  public static final class DefaultRequiresCsrfMatcher implements RequestMatcher {
+    private final Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+      return !allowedMethods.matcher(request.getMethod()).matches();
+    }
+  }
+}
--- src/main/java/com/zewde/newsdAuthentication/config/WebSecurityConfig.java
+++ src/main/java/com/zewde/newsdAuthentication/config/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -73,8 +74,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   public void configure(HttpSecurity http) throws Exception{
 
         http.cors()
-//            .and().csrf().disable()
-        .and().csrf().csrfTokenRepository(MyCsrfTokenRepository.withHttpOnlyFalse()).and()
+            .and().csrf().disable().addFilterBefore(
+            new MyCsrfFilter(), CsrfFilter.class)
+//        .and().csrf().csrfTokenRepository(MyCsrfTokenRepository.withHttpOnlyFalse()).and()
         .authorizeRequests()
             .antMatchers("/admin").hasAuthority("ADMIN")
             .antMatchers("/articles/*").hasAnyAuthority("USER","ADMIN")
@@ -111,7 +113,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     config.setAllowCredentials(true);
     config.setAllowedMethods(Arrays.asList("HEAD",
         "GET", "POST", "DELETE", "OPTIONS"));
-    config.setExposedHeaders(Arrays.asList("XSRF-TOKEN","X-XSRF-TOKEN","Set-Cookie"));
+    config.setExposedHeaders(Arrays.asList("XSRF-TOKEN","X-CSRF-TOKEN","X-XSRF-TOKEN","Set-Cookie"));
     config.setAllowedHeaders(Arrays.asList(
         "Authorization",
         "Accept",
@@ -123,6 +125,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         "Access-Control-Allow-Methods",
         "X-Frame-options",
         "X-XSRF-TOKEN",
+        "X-CSRF-TOKEN",
         "Strict-Transport-Security",
         "Content-Security-Policy",
         "X-Content-Type-Options",
